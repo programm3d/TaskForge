6666ffefa68dbca4db142eb53d81859d11a4fe9b
programm3d <pushansinha8@gmail.com>
2025-06-04 17:22:04 +0530

    Deployment Successful

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js
 import axios from "axios";
 
-const API_URL =  "http://localhost:8080/";
+const API_URL =  "https://taskforge-5282.onrender.com/";
 
 const api = axios.create({
   baseURL: API_URL,
